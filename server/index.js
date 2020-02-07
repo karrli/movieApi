6a6b52095035b108e54da1d0c7641dbef0713653
karrli <krla.ici@gmail.com>
2020-02-06 21:47:29 -0600

    primer commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT ||6660;
+const URL_MONGO = "mongodb+srv://karla:cintaroja@cluster0-ujohh.mongodb.net/test?retryWrites=true&w=majority";
+
+const movies = require('./controllers/movies.js');
+
+app.use(express.json());
+app.use(cors());
+app.use('/movies', movies);
+
+mongoose.connect(URL_MONGO,{ useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
+    if(!error){
+        console.log('Connected to MongoDB');
+    } else {
+        console.log(error);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server initialized on PORT ${PORT}`);
+});
